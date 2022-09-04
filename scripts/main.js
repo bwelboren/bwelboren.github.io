56ee5b9a2394fed4c52ec1e911ee00d7a39bd098
bwelboren <52087548+bwelboren@users.noreply.github.com>
2022-09-04 17:19:09 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,1 @@
-const listItem = document.querySelector("li:last-child");
-const newItem = document.createElement('li');
-newItem.innerHTML = '<a href="/products">Products</a>';
-listItem.parentNode.replaceChild(newItem, listItem);
+console.log("Sup");
